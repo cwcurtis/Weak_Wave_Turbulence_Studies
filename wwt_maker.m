8722ed59a9733b0318b436d5786e5a4a20c01a4f
Chris Curtis <ccurtis@mail.sdsu.edu>
2018-05-31 16:59:41 -0700

    HUh

--- wwt_maker.m
+++ wwt_maker.m
@@ -26,6 +26,7 @@ function wwt_maker(K,Llx,tf)
     Lap = 1i*(Dx2+Dy2) - nuh*Dhyp - nul*iDhyp;
     Eop = exp(dt*Lap);
 
+    %{
     f0 = ones(KT);
     f0(1:K-Kh-1,:) = 0;
     f0(K+Kh+1:end,:) = 0;
@@ -35,9 +36,25 @@ function wwt_maker(K,Llx,tf)
     for ll = K-Kl:K+Kl
         f0(K-Kl:K+Kl,ll) = 0;
     end
-   
+    %}
+    
+    f0 = zeros(KT);
+    
+    ksq = sqrt( ( (kron(ones(KT,1),(-K+1:K)')).^2 + (kron((-K+1:K)',ones(KT,1))).^2)/2 );
+    indsl = ksq >= Kl;
+    indsh = ksq <= Kh;
+    indsc = logical(indsl.*indsh);
+    f0(indsc) = 1;
+    f0 = reshape(f0,KT,KT);
     f0 = f0c*fftshift(f0);
-
+   
+    %{
+    un = abs(randn(KT));
+    phase = exp(1i*2*pi*rand(KT));
+    un = un.*phase;
+    un = 1e-4*un/norm(un);
+    un = un(:);
+    %}
     un = zeros(KT^2,1);
     uavg = zeros(KT^2,1);
     Nstart = 10000;
@@ -50,7 +67,7 @@ function wwt_maker(K,Llx,tf)
         un = Eop.*(un+k1/2) + k2/2;
         
         if jj> Nstart && mod(jj,Nint)==0
-            uavg = uavg + abs(un.*conj(un));
+            uavg = uavg + KT^4*abs(un.*conj(un));
             acnt = acnt + 1;
         end
     end
@@ -59,15 +76,19 @@ function wwt_maker(K,Llx,tf)
         uavg = fftshift(reshape(uavg/acnt,KT,KT));
         [krad,kavg] = mat_avg(uavg,K);
         figure(1)
-        plot(log10(krad),log10(kavg),'k-','LineWidth',2)
+        plot(log10(pi*krad/Llx),log10(2*Llx*krad.*kavg),'k-','LineWidth',2)
     end
     
     Xmesh = linspace(-Llx,Llx,KT+1);
     Xmesh = Xmesh(1:KT)';
+    ufin = ifft2(reshape(un,KT,KT));
     
     figure(2)
-    imagesc(Xmesh,Xmesh,abs(ifft2(reshape(un,KT,KT))))
+    imagesc(Xmesh,Xmesh,abs(ufin))
     
+    figure(3)
+    imagesc(Xmesh,Xmesh,angle(ufin))
+        
     toc
 end
 
@@ -82,13 +103,17 @@ end
 function [krad,kavg] = mat_avg(M,K)
     KT = 2*K;
     inds = -K+1:K;
+    figure(4)
+    surf(inds,inds,log10(M),'LineStyle','none')
+    
+    krad = (1:K)';
+    kavg = M(K,K+1:KT)';
+    %{
     indsr = kron(ones(KT,1),inds');
     indsc = kron(inds',ones(KT,1));
     M = M(:);
-    dk = pi/K;
-    krad = linspace(dk,pi,K);
     kavg = zeros(length(krad),1);
-    mrads = dk*sqrt((indsr.^2+indsc.^2)/2);
+    mrads = sqrt((indsr.^2+indsc.^2)/2);
     for jj=1:length(krad)-1
        indsl = mrads >= krad(jj);
        indsh = mrads < krad(jj+1);
@@ -99,4 +124,6 @@ function [krad,kavg] = mat_avg(M,K)
           kavg(jj) = sum(M(indsc))/totparts; 
        end
     end
+    %}
+    
 end
